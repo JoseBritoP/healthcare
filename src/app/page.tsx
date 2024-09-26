88561aad55bd7ae254781886d6dc3184b966c7a8
José Brito <josealbritose@gmail.com>
2024-09-26 17:55:25 -0300

    HomePage init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import PatientForm from "@/components/shared/Forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-cintainer max-w-[496px]">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+            alt="CarePulse Logo"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src={"/assets/images/onboarding-img.png"}
+        alt="Doctors"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
-  )
+  );
 }
