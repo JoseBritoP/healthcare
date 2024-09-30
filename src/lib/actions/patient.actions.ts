5fab012d74b0eedd164264af7a656a577064b0e0
José Brito <josealbritose@gmail.com>
2024-09-30 19:30:45 -0300

    use server

--- src/lib/actions/patient.actions.ts
+++ src/lib/actions/patient.actions.ts
@@ -1,3 +1,4 @@
+"use server"
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Query,ID } from "node-appwrite";
 import { users } from "../appwrite-config";
@@ -6,13 +7,14 @@ import { parseStringify } from "../utils";
 export const createUser = async (user:CreateUserParams) => {
   try {
     const newUser = await users.create(ID.unique(),user.email,user.phone, undefined,user.name)
-    return parseStringify(newUser);
+    const userAppWrite = await parseStringify(newUser);
+    return userAppWrite
   } catch (error:any) {
+    console.log('Error',error)
     if(error && error?.code===409){
       const documents = await users.list([
         Query.equal('email',[user.email])
       ]);
-      
       return documents?.users[0]
     }
   }
