5893942ded3e2405ae74e0b9950fcd26777473dc
José Brito <josealbritose@gmail.com>
2024-10-03 17:57:09 -0300

    Clean page

--- src/app/patients/[userId]/register/page.tsx
+++ src/app/patients/[userId]/register/page.tsx
@@ -1,19 +1,21 @@
 import RegisterForm from "@/components/shared/Forms/RegisterForm/RegisterForm";
 import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
-export default async function RegisterPage({params}:{params:{userId:string}}) {
-
-  const user = await getUser(params.userId)
-  if(!user) redirect('/');
+export default async function RegisterPage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const user = await getUser(params.userId);
+  if (!user) redirect("/");
   return (
     <div className="flex h-screen max-h-screen">
       {/* TODO: OTP Verification */}
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-cintainer max-w-[496px]">
+      <section className="container">
+        <div className="sub-cintainer max-w-[860px] flex-1 flex-col py-10">
           <Image
             src={"/assets/icons/logo-full.svg"}
             height={1000}
@@ -22,16 +24,11 @@ export default async function RegisterPage({params}:{params:{userId:string}}) {
             alt="CarePulse Logo"
           />
 
-          <RegisterForm user={user}/>
+          <RegisterForm user={user} />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePulse
-            </p>
-            <Link href={"/?admin=true"} className="text-green-500">
-              Admin
-            </Link>
-          </div>
+          <p className="copyright py-12">
+            © 2024 CarePulse
+          </p>
         </div>
       </section>
       <Image
