c2b5d9cab5914ef7512db5940579e2654b1725e1
José Brito <josealbritose@gmail.com>
2024-10-13 09:23:58 -0300

    Clean AppointmentForm

--- src/components/shared/Forms/Appointment/AppointmentForm.tsx
+++ src/components/shared/Forms/Appointment/AppointmentForm.tsx
@@ -1,129 +1,25 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { Dispatch, SetStateAction, useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { SelectItem } from "@/components/ui/select";
 import { Doctors } from "@/constants";
-import { getAppointmentSchema } from "@/lib/validation";
 import { Appointment } from "@/types/appwrite.types";
 import "react-datepicker/dist/react-datepicker.css";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
-import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actionts";
 import { Form } from "@/components/ui/form";
+import useAppointmentForm from "@/hooks/useAppointmentForm";
 
-export const AppointmentForm = ({
-  userId,
-  patientId,
-  type = "create",
-  appointment,
-  setOpen,
-}: {
+export interface AppointmentFormProps {
   userId: string;
   patientId: string;
   type: "create" | "schedule" | "cancel";
   appointment?: Appointment;
-  setOpen?: Dispatch<SetStateAction<boolean>>;
-}) => {
+}
 
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-  
-  const AppointmentFormValidation = getAppointmentSchema(type);
-  
-  const form = useForm<z.infer<typeof AppointmentFormValidation>>({
-    resolver: zodResolver(AppointmentFormValidation),
-    defaultValues: {
-      primaryPhysician: appointment ? appointment?.primaryPhysician : "",
-      schedule: appointment
-      ? new Date(appointment.schedule!)
-      : new Date(Date.now()),
-      reason: appointment ? appointment.reason : "",
-      note: appointment?.note || "",
-      cancellationReason: appointment?.cancellationReason || "",
-    },
-  });
+export const AppointmentForm = ({ userId,patientId,type = "create",appointment} : AppointmentFormProps) => {
 
-  if(!appointment) return;
-  
-  const onSubmit = async (
-    values: z.infer<typeof AppointmentFormValidation>
-  ) => {
-    setIsLoading(true);
-
-    let status;
-    switch (type) {
-      case "schedule":
-        status = "scheduled";
-        break;
-      case "cancel":
-        status = "cancelled";
-        break;
-      default:
-        status = "pending";
-    }
-
-    try {
-      if (type === "create" && patientId) {
-        const appointment = {
-          userId,
-          patient: patientId,
-          primaryPhysician: values.primaryPhysician,
-          schedule: new Date(values.schedule),
-          reason: values.reason!,
-          status: status as Status,
-          note: values.note,
-        };
-
-        const newAppointment = await createAppointment(appointment);
-
-        if (newAppointment) {
-          form.reset();
-          router.push(
-            `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
-          );
-        }
-      } else {
-        const appointmentToUpdate = {
-          userId,
-          appointmentId: appointment.$id!,
-          appointment: {
-            primaryPhysician: values.primaryPhysician,
-            schedule: new Date(values.schedule),
-            status: status as Status,
-            cancellationReason: values.cancellationReason,
-          },
-          type,
-        };
-
-        const updatedAppointment = await updateAppointment(appointmentToUpdate);
-
-        if (updatedAppointment) {
-          setOpen && setOpen(false);
-          form.reset();
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    setIsLoading(false);
-  };
-
-  let buttonLabel;
-  switch (type) {
-    case "cancel":
-      buttonLabel = "Cancel Appointment";
-      break;
-    case "schedule":
-      buttonLabel = "Schedule Appointment";
-      break;
-    default:
-      buttonLabel = "Submit Apppointment";
-  }
+  const { isLoading, buttonLabel, onSubmit,form } = useAppointmentForm({userId,patientId,type,appointment})
 
   return (
     <Form {...form}>
@@ -147,7 +43,7 @@ export const AppointmentForm = ({
               placeholder="Select a doctor"
             >
               {Doctors.map((doctor, i) => (
-                <SelectItem key={doctor.name + i} value={doctor.name}>
+                <SelectItem key={doctor.name + i} value={doctor.name} className="hover:bg-gray-700 transition-colors duration-200 ease-in">
                   <div className="flex cursor-pointer items-center gap-2">
                     <Image
                       src={doctor.image}
