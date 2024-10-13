19bb96ab2c37454a545baf75a7d3bf4ec89ab492
José Brito <josealbritose@gmail.com>
2024-10-13 09:24:14 -0300

    Custom hook for AppointmentForm

---
+++ src/hooks/useAppointmentForm.tsx
@@ -0,0 +1,108 @@
+"use client"
+import { z } from "zod";
+import { AppointmentFormProps } from '@/components/shared/Forms/Appointment/AppointmentForm';
+import { useState } from 'react';
+import { getAppointmentSchema } from '@/lib/validation';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { createAppointment, updateAppointment } from '@/lib/actions/appointment.actionts';
+
+export default function useAppointmentForm({userId,patientId,type,appointment}:AppointmentFormProps) {
+
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const AppointmentFormValidation = getAppointmentSchema(type);
+
+  const form = useForm<z.infer<typeof AppointmentFormValidation>>({
+    resolver: zodResolver(AppointmentFormValidation),
+    defaultValues: {
+      primaryPhysician: appointment ? appointment?.primaryPhysician : "",
+      schedule: appointment
+      ? new Date(appointment.schedule!)
+      : new Date(Date.now()),
+      reason: appointment ? appointment.reason : "",
+      note: appointment?.note || "",
+      cancellationReason: appointment?.cancellationReason || "",
+    },
+  });
+
+  
+  const onSubmit = async (values: z.infer<typeof AppointmentFormValidation>) => {
+    setIsLoading(true);
+
+    let status;
+    switch (type) {
+      case "schedule":
+        status = "scheduled";
+        break;
+      case "cancel":
+        status = "cancelled";
+        break;
+      default:
+        status = "pending";
+    }
+
+    try {
+      if (type === "create") {
+        const appointment = {
+          userId,
+          patient: patientId,
+          primaryPhysician: values.primaryPhysician,
+          schedule: new Date(values.schedule),
+          reason: values.reason!,
+          status: status as Status,
+          note: values.note,
+        };
+
+        const newAppointment = await createAppointment(appointment);
+
+        if (newAppointment) {
+          form.reset();
+          router.push(
+            `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
+          );
+        }
+      } else {
+        if(appointment === undefined) throw new Error(`Appointment Id is undefined`);
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment.$id,
+          appointment: {
+            primaryPhysician: values.primaryPhysician,
+            schedule: new Date(values.schedule),
+            status: status as Status,
+            cancellationReason: values.cancellationReason,
+          },
+          type,
+        };
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          form.reset();
+        }
+      }
+    } catch (error) {
+      console.log('An error ocurred creating appointment',error);
+    }
+    setIsLoading(false);
+  };
+
+  let buttonLabel='test';
+  switch (type) {
+    case "cancel":
+      buttonLabel = "Cancel Appointment";
+      break;
+    case "schedule":
+      buttonLabel = "Schedule Appointment";
+      break;
+    default:
+      buttonLabel = "Submit Apppointment";
+  }
+
+
+  return { isLoading, buttonLabel, onSubmit, form }
+
+}
